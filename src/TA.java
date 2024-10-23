12c6017954cd5d6bebd833f6d34af847b6ca7d07
Harini Ravi <harinirh5@gmail.com>
2024-10-23 19:22:13 -0400

    skeleton java for TA

--- src/TA.java
+++ src/TA.java
@@ -194,7 +194,7 @@ public class TA {
             case 1:
                 //redirect to add section
                 System.out.println("Success.");
-                this.addSection("add", courseID, chapterID, chapterTitle);
+                this.addSection("addChapter", courseID, chapterID, chapterTitle);
                 break;
             case 2:
                 //redirect to previous page
@@ -220,7 +220,7 @@ public class TA {
             case 1:
                 //redirect to add section
                 System.out.println("Success.");
-                this.addSection("modify", courseID, chapterID, "");
+                this.addSection("modifyChapter", courseID, chapterID, "");
                 break;
             case 2:
                 //redirect to add activity
@@ -250,14 +250,16 @@ public class TA {
         switch(choice) {
             case 1:
                 //redirect to add content block
-                this.addContentBlock(callingFunction, courseID, chapterID, chapterTitle);
+                this.addContentBlock("addSection", courseID, chapterID, chapterTitle);
                 break;
             case 2:
                 //redirect to previous page
-                if(callingFunction.equals("add")) {
+                if(callingFunction.equals("addSection")) {
                     this.addChapter(courseID);
-                } else {
+                } else if(callingFunction.equals("modifyChapter")) {
                     this.modifyChapter(courseID);
+                } else {
+                    this.landing();
                 }
                 break;
             default:
@@ -309,7 +311,13 @@ public class TA {
                 break;
             case 5:
                 //redirect to previous menu (add section menu)
-                this.addSection(callingFunction, courseID, chapterID, chapterTitle);
+                if(callingFunction.equals("addSection")) {
+                    this.addSection("addContentBlock", courseID, chapterID, chapterTitle);
+                } else if(callingFunction.equals("modifySection")) {
+                    this.modifyContentBlock("addContentBlock", courseID, chapterID, chapterTitle);
+                } else {
+                    this.landing();
+                }
                 break;
             default:
                 System.out.println("Invalid entry, exiting application.");
@@ -318,5 +326,147 @@ public class TA {
         }
     }
 
+    //function to modify section
+    private void modifySection(String callingFunction, String courseID, String chapterID, String chapterTitle) {
+        System.out.println("Enter section number: ");
+        String sectionNum = scanner.nextLine();
+        System.out.println("Enter section title: ");
+        String sectionTitle = scanner.nextLine();
+        System.out.println("Enter chapter ID: ");
+        String chapterId = scanner.nextLine();
+        System.out.println("Enter book: ");
+        String bookId = scanner.nextLine();
 
+        System.out.println("1. Add new content block\n2. Modify content block\n3. Delete content block\n4. Hide content block\n5. Go back");
+        System.out.println("Enter your choice (1-5): ");
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                //redirect to add content block
+                this.addContentBlock("modifySection", courseID, chapterID, chapterTitle);
+                break;
+            case 2:
+                //redirect to modify content block
+                this.modifyContentBlock("modifySection", courseID, chapterID, chapterTitle);
+                break;
+            case 3:
+                //redirect to delete content block
+                this.deleteContentBlock("modifySection", courseID, chapterID, chapterTitle);
+                break;
+            case 4:
+                //redirect to hide content block
+                break;
+            case 5:
+                //redirect to previous page (unknown flow)
+                break;
+            default:
+                System.out.println("Invalid entry, exiting application.");
+                System.exit(0);
+                break;
+        }
+    }
+
+    //function to modify content block
+    private void modifyContentBlock(String callingFunction, String courseID, String chapterID, String chapterTitle) {
+        System.out.println("Enter content block ID: ");
+        String contentId = scanner.nextLine();
+
+        System.out.println("1. Add text\n2. Add picture\n3. Add activity\n4. Hide activity\n5. Go back\n6. Landing Page");
+        System.out.println("Enter your choice (1-6): ");
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                //redirect to add text
+                String text = helper.getText();
+                if(!text.isEmpty()) {
+                    //TODO: handle adding text
+                } else {
+                    this.addContentBlock(callingFunction, courseID, chapterID, chapterTitle);
+                }
+                break;
+            case 2:
+                //redirect to add picture
+                String picture = helper.getPicture();
+                if(!picture.isEmpty()) {
+                    //TODO: handle adding picture
+                } else {
+                    this.addContentBlock(callingFunction, courseID, chapterID, chapterTitle);
+                }
+                break;
+            case 3:
+                //redirect to add activity
+                Map<String, String> activity = helper.getActivity();
+                if(activity.isEmpty()) {
+                    this.addSection(callingFunction, courseID, chapterID, chapterTitle);
+                } else {
+                    //TODO: handle adding activity
+                }
+                break;
+            case 4:
+                //redirect to hide activity
+                break;
+            case 5:
+                //redirect to previous menu (add section menu)
+                this.addSection("modifyContentBlock", courseID, chapterID, chapterTitle);
+                break;
+            case 6:
+                //redirect to landing page
+                this.landing();
+                break;
+            default:
+                System.out.println("Invalid entry, exiting application.");
+                System.exit(0);
+                break;
+        }
+    }
+
+    //function to delete content block
+    private void deleteContentBlock(String callingFunction, String courseID, String chapterID, String chapterTitle) {
+        System.out.println("Enter content block ID: ");
+        String contentId = scanner.nextLine();
+
+        System.out.println("1. Delete\n2. Go back");
+        System.out.println("Enter your choice (1-2): ");
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                //TODO: handle deletion
+                break;
+            case 2:
+                //redirect to previous page (modify section)
+                this.modifySection(callingFunction, courseID, chapterID, chapterTitle);
+                break;
+            default:
+                System.out.println("Invalid entry, exiting application.");
+                System.exit(0);
+                break;
+        }
+    }
+
+    //function to hide content block
+    private void hideContentBlock(String callingFunction, String courseID, String chapterID, String chapterTitle) {
+        System.out.println("Enter content block ID: ");
+        String contentId = scanner.nextLine();
+
+        System.out.println("1. Hide\n2. Go back");
+        System.out.println("Enter your choice (1-2): ");
+        int choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                //TODO: handle hiding
+                break;
+            case 2:
+                //redirect to previous page (modify section)
+                this.modifySection(callingFunction, courseID, chapterID, chapterTitle);
+                break;
+            default:
+                System.out.println("Invalid entry, exiting application.");
+                System.exit(0);
+                break;
+        }
+    }
 }
