f48b126e00d174f7ae46769cde9094b27de9ba41
sakshibasapure <sakshibasapure2@gmail.com>
2024-10-29 19:39:02 -0400

    Added Chapter entity and repository

---
+++ src/Entity/Chapter.java
@@ -0,0 +1,61 @@
+package Entity;
+
+public class Chapter {
+    private int id;
+    private int textbookId;
+    private String title;
+    private Boolean visibility;
+
+    // Constructors
+    public Chapter() {}
+
+    public Chapter(int id, int textbookId, String title, Boolean visibility) {
+        this.id = id;
+        this.textbookId = textbookId;
+        this.title = title;
+        this.visibility = visibility;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTextbookId() {
+        return textbookId;
+    }
+
+    public void setTextbookId(int textbookId) {
+        this.textbookId = textbookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(Boolean visibility) {
+        this.visibility = visibility;
+    }
+
+    @Override
+    public String toString() {
+        return "Chapter{" +
+                "id=" + id +
+                ", textbookId=" + textbookId +
+                ", title='" + title + '\'' +
+                ", visibility=" + visibility +
+                '}';
+    }
+}
