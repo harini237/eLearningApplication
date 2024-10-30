04634993ace85dbbea6ade505cacbb08a9e9363a
Harini Ravi <harinirh5@gmail.com>
2024-10-29 21:14:37 -0400

    created course related repositories

---
+++ src/Repository/PendingApprovalRepository.java
@@ -0,0 +1,79 @@
+package Repository;
+
+import Entity.PendingApproval;
+import Util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PendingApprovalRepository {
+    public void createApproval (PendingApproval approval) {
+        String sql = "INSERT INTO pending_approval (course_token, student_id)" +
+                "VALUES (?, ?)";
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, approval.getCourse_token());
+            pstmt.setString(2, approval.getStudent_id());
+
+            pstmt.executeUpdate();
+            System.out.println("Student "+ approval.getStudent_id()+ " pending approval for "+ approval.getCourse_token());
+        } catch (SQLException e) {
+            System.out.println("Could not create approval request.");
+        }
+    }
+
+    public List<PendingApproval> findApprovalsByCourse (String course_token) {
+        List<PendingApproval> approvals = new ArrayList<>();
+
+        String sql = "SELECT course_token, student_id FROM pending_approval" +
+                "WHERE course_token = ?";
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, course_token);
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                PendingApproval approval = new PendingApproval(rs.getString("student_id"),rs.getString("course_token"));
+                approvals.add(approval);
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not find pending approvals.");
+        }
+
+        return approvals;
+    }
+
+    public List<PendingApproval> findApprovalsByStudent (String student_id) {
+        List<PendingApproval> approvals = new ArrayList<>();
+
+        String sql = "SELECT course_token, student_id FROM pending_approval" +
+                "WHERE student_id = ?";
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, student_id);
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                PendingApproval approval = new PendingApproval(rs.getString("student_id"),rs.getString("course_token"));
+                approvals.add(approval);
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not find pending approvals.");
+        }
+
+        return approvals;
+    }
+}
