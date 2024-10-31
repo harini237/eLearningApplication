70ab02b693ff816f6db68318ccb6e0b039af4e03
Harini Ravi <harinirh5@gmail.com>
2024-10-31 18:55:18 -0400

    updated db connection in Main

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,20 @@ import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
-        //load oracle driver
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            String url = "jdbc:oracle:localhost:8080";
+        //load mariadb driver
+        String url = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/";
 
-            //establish jdbc connection
-            Connection con;
-            String user = "user";
-            String pass = "pass";
-            try {
-                con = DriverManager.getConnection(url, user, pass);
-                new Home(con);
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Could not establish jdbc connection.");
-            }
-        } catch (ClassNotFoundException c) {
-            System.out.println("Could not find Oracle Driver class.");
+        //establish jdbc connection
+        Connection con;
+        String user = "";
+        String pass = "";
+        try {
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Successfully connected!");
+            new Home(con);
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Could not establish jdbc connection.");
         }
     }
 }
