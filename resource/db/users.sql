c1767d234f47791f2a6b5220dcd146afe2e1ea9c
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-11-03 16:17:37 -0500

    Added Insert Queries for users.sql

---
+++ resource/db/users.sql
@@ -0,0 +1,65 @@
+--- Faculty ---
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('DaBr1024', 'David', 'Brown', 'david.b@webmail.com', 'c1bdb6315c8cf215303c839451a45ae9e5a8b794a1427b4e1d080d1d417201d2', '2024-10-03 13:56:22.000', NULL, 2, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('EmDa1024', 'Emily', 'Davis', 'emily.davis@email.com', '108f06be0302af9fe81e0bbb4a8c0709f1c1233264e543d26471f0a874c015c6', '2024-10-03 13:56:22.000', NULL, 2, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('JoDo1024', 'John', 'Doe', 'john.doe@example.com', 'ccab703b592a5f8e9dd6baf46a38ad54b75c1e6857fd1f3eb49a3f8ce1d123df', '2024-10-03 13:56:22.000', NULL, 2, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('KeOg1024', 'Kemafor', 'Ogan', 'kogan@ncsu.edu', '2880e85e38cf8a7fcbb199b6fcbbd0571818ddfbbfc37f5a239b4e6d0d6deff8', '2024-10-03 13:56:22.000', NULL, 2, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('MiWi1024', 'Michael', 'Wilson', 'michael.w@service.com', '38f83823ceecddb6b136f0e1c13259c33305435c20795057a1ff6f6d630d133a', '2024-10-03 13:56:22.000', NULL, 2, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('SaMi1024', 'Sarah', 'Miller', 'sarah.miller@domain.com', 'd46879282bbeabf73626330f96945b1ad7a063850146df5114841af03219302f', '2024-10-03 13:56:22.000', NULL, 2, 0);
+
+--- TA ---
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('DaJo1024', 'David', 'Johnson', 'djohnson@ncsu.edu', 'cd498695fff11b3cc412a5fa1fe453f213a7732197d4ec2cc45e7341d3897124', '2024-10-03 13:56:22.000', NULL, 3, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('ElCl1024', 'Ellie', 'Clark', 'eclark@ncsu.edu', '332594ebbcbdfd1ee5c0de2402fadc6e3ef5a24521e38ae1dc457542f27159e6', '2024-10-03 13:56:22.000', NULL, 3, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('JaWi1024', 'James', 'Williams', 'jwilliams@ncsu.edu', 'dd0c2ffd09cca8aac28d3c9d095a59b2e885dee17b63cc4c98114a069ac63b1a', '2024-11-03 13:56:22.000', NULL, 3, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('JeGi0924', 'Jeff', 'Gibson', 'jgibson@ncsu.edu', '3bce8a55eb3e1a6773b629cf010a5c6d5c3e872e7e35811957019d2ba425bb90', '2024-09-03 13:56:22.000', NULL, 3, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('LiAl0924', 'Lisa', 'Alberti', 'lalberti@ncsu.edu', '4651846ac10063cb5746b0a59d2b84da92f9eca6859b329a73eef26318b778ea', '2024-09-03 13:56:22.000', NULL, 3, 0);
+
+--- Student ---
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('AlAr1124', 'Alice', 'Artho', 'aa23@edu.mail', '58f64ffc974e95398f0547c0b74de39522027c911fcbde1bc20ebced9e5b67db', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('ArMo1124', 'Aria', 'Morrow', 'am213@example.edu', '1d28df982c83fd3849f15900dce5f48ab1fd6c90da5547bb12bda02db50e37d3', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('BoTe1124', 'Bob', 'Temple', 'bt163@template.mail', 'a91cceeec4d0c1c818a434b8b5001b6615aa47c5e10a87c6a0f536ab32780b7c', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('ErPe1124', 'Eric', 'Perrig', 'ez356@example.mail', '82a3a98f07a238945a0122c9235f641ed424985b71f60607a670133b1047f938', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('FiWi1124', 'Finn', 'Wilder', 'fw23@example.edu', '9ee0290e3e4895a1eace318926847d3587abe375261a2beed355235f7dbcbec4', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('KeRh1114', 'Kellan', 'Rhodes', 'kr21@example.edu', '388a5d114949ca6336d41eb9aebdef166710659070a8989900005ca71434e620', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('LeMe1124', 'Leona', 'Mercer', 'lm56@example.edu', '629235610f1a33a76a5a9be151fe92b7dd0b7f1ada3555f7102d18b03ca060a0', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('LiGa1124', 'Lily', 'Gaddy', 'li123@example.edu', '4c8ec4630f0208e3e7a9420aa97c8109aedff0f2a28193940b4db6b7fffe8caa', '2024-11-03 13:56:22.000', NULL, 4, 0);
+INSERT INTO `user`
+(id, first_name, last_name, email, password, created_at, created_by, role_id, is_pwd_reset_req)
+VALUES('SiHa1124', 'Sienna', 'Hayes', 'sh13@example.edu', 'b224894aaaf3d5edd9ec799e5bb0edcd0bcef44440ac833c2c83d82de0a2b89d', '2024-11-03 13:56:22.000', NULL, 4, 0);
