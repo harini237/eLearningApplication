ad6196a30815afeaa4c8c4c9289e5d3bbd4b6699
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-11-03 14:19:14 -0500

    Added Login page and respective handlers

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
+import Util.DatabaseConnection;
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
-        //load mariadb driver
-        String url = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/";
-
-        //establish jdbc connection
-        Connection con;
-        String user = "";
-        String pass = "";
         try {
-            con = DriverManager.getConnection(url, user, pass);
+            Connection con = DatabaseConnection.getConnection();
             System.out.println("Successfully connected!");
             new Home(con);
             con.close();
