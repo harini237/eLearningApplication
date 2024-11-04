b6d2c316dc3135fb4dde533e2180f86f92826dcc
Harini Ravi <harinirh5@gmail.com>
2024-11-03 20:08:55 -0500

    added course services for faculty menu ops

--- src/Repository/EnrollmentRepository.java
+++ src/Repository/EnrollmentRepository.java
@@ -8,7 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EnrollmentRepository {
     public void createEnrollment (Enrollment enrollment) {
@@ -28,7 +30,7 @@ public class EnrollmentRepository {
         }
     }
 
-    public List<String> findCoursesByStudent (String student_id) {
+    public List<String> findCourseTokenByStudent (String student_id) {
         List<String> course_tokens = new ArrayList<>();
 
         String sql = "SELECT course_token FROM enrollment" +
@@ -51,7 +53,7 @@ public class EnrollmentRepository {
         return course_tokens;
     }
 
-    public List<String> findStudentsByCourse (String course_token) {
+    public List<String> findStudentsByCourseToken (String course_token) {
         List<String> students = new ArrayList<>();
 
         String sql = "SELECT student_id FROM enrollment" +
@@ -73,4 +75,43 @@ public class EnrollmentRepository {
 
         return students;
     }
+
+    public Map<String,List<String>> findEnrolledStudentsByFaculty(String faculty_id) {
+        String sql = "SELECT E.student_id, C.id" +
+                "FROM enrollment E" +
+                "JOIN active_course A" +
+                "ON E.course_token = A.token" +
+                "JOIN course C" +
+                "ON A.course_id = C.id" +
+                "WHERE faculty_id = ?";
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, faculty_id);
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String stu = rs.getString("student_id");
+                String course = rs.getString("course_id");
+                if (map.containsKey(course)) {
+                    List<String> stu_list = map.get(course);
+                    stu_list.add(stu);
+                    map.replace(course, stu_list);
+                } else {
+                    List<String> stu_list = new ArrayList<>();
+                    stu_list.add(stu);
+                    map.put(course,stu_list);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not find enrolled students.");
+        }
+
+        return map;
+    }
+
 }
