5c43a47248a82ca3068e93e4520f929f198635fd
Harini Ravi <harinirh5@gmail.com>
2024-11-05 14:43:31 -0500

    modified student menu

--- src/Util/DatabaseConnection.java
+++ src/Util/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mariadb://localhost:3306/jjanart";
-    private static final String USERNAME = "your_db_username";
-    private static final String PASSWORD = "your_db_password";
+    private static final String URL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/<unity id>";
+    private static final String USERNAME = "<unity id>";
+    private static final String PASSWORD = "<db password>";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
