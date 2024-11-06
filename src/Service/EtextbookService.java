b43dc13a2b4250fa1b8fe3e5168bc0f9eefddd80
sakshibasapure <sakshibasapure2@gmail.com>
2024-11-06 10:55:09 -0500

    formating index

--- src/Service/EtextbookService.java
+++ src/Service/EtextbookService.java
@@ -102,24 +102,21 @@ public class EtextbookService {
         }
     }
     
-    
-    
+    public void deleteSection(int textbookId, String chapterId, String sectionId) {
+        try {
+            etextbookRepository.deleteSection(textbookId, chapterId, sectionId);
+        } catch (Exception e) {
+            System.err.println("Error deleting section: " + e.getMessage());
+        }
+    }
 
-        public void deleteSection(int textbookId, String chapterId, String sectionId) {
-            try {
-                etextbookRepository.deleteSection(textbookId, chapterId, sectionId);
-            } catch (Exception e) {
-                System.err.println("Error deleting section: " + e.getMessage());
-            }
+    public void hideSection(int textbookId, String chapterId, String sectionId) {
+        try {
+            etextbookRepository.hideSection(textbookId, chapterId, sectionId);
+        } catch (Exception e) {
+            System.err.println("Error hiding section: " + e.getMessage());
         }
-    
-        public void hideSection(int textbookId, String chapterId, String sectionId) {
-            try {
-                etextbookRepository.hideSection(textbookId, chapterId, sectionId);
-            } catch (Exception e) {
-                System.err.println("Error hiding section: " + e.getMessage());
-            }
-        }
+    }
 
 }
     
