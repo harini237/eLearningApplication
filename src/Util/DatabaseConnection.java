e932dbe5c6cdc6211d81afcdd59afcbe55cbfd5a
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-11-06 23:38:52 -0500

    Update DB Conn

--- src/Util/DatabaseConnection.java
+++ src/Util/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/jjanart";
-    private static final String USERNAME = "jjanart";
-    private static final String PASSWORD = "200537782";
+    private static final String URL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
